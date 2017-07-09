edf87f90106696a09e93486052c417f3be6b8569
Marcela Rubim <marcelarubimv@gmail.com>
2017-07-09 20:48:59 +0200

    OST enum values

--- app/models/ost.rb
+++ app/models/ost.rb
@@ -8,8 +8,12 @@ class Ost < ApplicationRecord
 
   accepts_nested_attributes_for :fluxes, allow_destroy: true, reject_if: :all_blank
 
-  enum status: []
-  enum type: []
-  enum service_method: []
+  enum status: [:in_progress, :complete, :no_return]
+  enum type: [:implantacao, :loc_temporaria, :manutencao_troca_producao,
+              :troca_equipamento, :edicao_holdcast, :migracao, :migracao_temporaria,
+              :apoio_tecnico, :recolhimento, :cancelado]
+  enum service_method: [:correio, :visita, :telefone, :web_servidor]
   enum repair_status: []
+  enum product_evaluation: [:a_instalar, :espera_ok, :mudo, :musica_interna,
+                            :ruido_chiado, :volume_alto, :volume_baixo, :desativado]
 end
