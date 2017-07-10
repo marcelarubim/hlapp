e55abba1033f6ef5ffb33c5afc31cf5698549ee9
Marcela Rubim <marcelarubimv@gmail.com>
2017-07-10 13:15:28 +0200

    Flux model specification

--- app/models/flux.rb
+++ app/models/flux.rb
@@ -1,2 +1,9 @@
+# .nodoc. #
 class Flux < ApplicationRecord
+  has_paper_trail
+  belongs_to :fluxable, polymorphic: true
+
+  def display
+    [created_at.strftime('%d/%m/%Y'), notes.truncate(56)].join(' - ')
+  end
 end
--- spec/factories/fluxes.rb
+++ spec/factories/fluxes.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :flux do
-    
+    association :fluxable, factory: :ost, strategy: :build
+    notes { Faker::Lorem.paragraph }
   end
 end
