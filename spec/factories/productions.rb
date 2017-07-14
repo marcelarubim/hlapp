03283086d6485fdf161b70005254825b47442adf
Marcela Rubim <marcelarubimv@gmail.com>
2017-07-14 23:28:52 +0200

    Fixing production factory airing_month and renew_forecast_month type stored

--- spec/factories/productions.rb
+++ spec/factories/productions.rb
@@ -4,6 +4,11 @@ FactoryGirl.define do
     theme { Faker::Commerce.color }
     details { Faker::Lorem.paragraph }
     airing_month { Faker::Date.between(Date.today, 240.days.from_now) }
-    renew_forecast_month { airing_month + rand(100...400) }
+    renew_forecast_month { (airing_month + rand(100...400)).strftime('%Y-%m') }
+
+    after(:create) do |production|
+      production.airing_month = production.airing_month.to_date.strftime('%Y-%m')
+      production.save
+    end
   end
 end
